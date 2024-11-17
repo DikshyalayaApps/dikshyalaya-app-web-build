023258f062f9851cea4897cec47335cfa90f1298
Alien Shrestha <npalien4u@gmail.com>
2024-11-17 20:05:19 +0530

    fix check for env file

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "376d96db6235c4208b9d357906a956cb
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "6b2968679158a0aa0a66f4d368723981",
+"flutter_bootstrap.js": "ae0dadaa01e5d2d1a386f39430e3b273",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
